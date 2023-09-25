1ece2aa097283f3cc2d32e7508ca9341f68d6773
patricksky006 <patrick.sky@hotmail.com>
2023-09-25 13:23:55 +0800

    Finished the test

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,10 +1,9 @@
-const { Restaurant } = require('../models')
-const { User } = require('../models')
+const { Restaurant, User } = require('../models')
 const { localFileHandler } = require('../helpers/file-helpers')
 
 const adminController = {
   getRestaurants: (req, res, next) => {
-    Restaurant.findAll({
+    return Restaurant.findAll({
       raw: true
     })
       .then(restaurants => res.render('admin/restaurants', { restaurants }))
@@ -33,7 +32,7 @@ const adminController = {
       .catch(err => next(err))
   },
   getRestaurant: (req, res, next) => {
-    Restaurant.findByPk(req.params.id, {
+    return Restaurant.findByPk(req.params.id, {
       raw: true
     })
       .then(restaurant => {
@@ -43,7 +42,7 @@ const adminController = {
       .catch(err => next(err))
   },
   editRestaurant: (req, res, next) => {
-    Restaurant.findByPk(req.params.id, {
+    return Restaurant.findByPk(req.params.id, {
       raw: true
     })
       .then(restaurant => {
@@ -87,60 +86,29 @@ const adminController = {
       .catch(err => next(err))
   },
   getUsers: (req, res, next) => {
-    User.findAll({
+    return User.findAll({
       raw: true
     })
-      .then(Users => res.render('admin/users', { Users }))
+      .then(users => res.render('admin/users', { users }))
       .catch(err => next(err))
   },
-  getUser: (req, res, next) => {
-    User.findByPk(req.params.id, {
-      raw: true
-    })
-      .then(User => {
-        if (!User) throw new Error("User didn't exist!")
-        console.log(User)
-        res.render('admin/user', { User })
-      })
-      .catch(err => next(err))
-  },
-  editUser: (req, res, next) => {
-    User.findByPk(req.params.id, {
-      raw: true
-    })
-      .then(user => {
-        if (!user) throw new Error("user didn't exist!")
-        res.render('admin/edit-user', { user })
-      })
-      .catch(err => next(err))
-  },
-  putUser: (req, res, next) => {
-    const { name, email, isAdmin } = req.body
-    console.log(req.body)
-    if (!name) throw new Error('User name is required!')
-    if (!email) throw new Error('User email is required!')
-    User.findByPk(req.params.id)
+  patchUser: (req, res, next) => {
+    const id = req.params.id
+    return User.findByPk(id)
       .then(user => {
         if (!user) throw new Error("User didn't exist!")
+        if (user.email === 'root@example.com') {
+          req.flash('error_messages', '禁止變更 root 權限')
+          return res.redirect('back')
+        }
         return user.update({
-          name,
-          email,
-          isAdmin: isAdmin === 'adminOn'
+          isAdmin: !user.isAdmin
         })
       })
       .then(() => {
-        req.flash('success_messages', 'User was successfully to update')
-        res.redirect('/admin/users')
+        req.flash('success_messages', '使用者權限變更成功')
+        return res.redirect('/admin/users')
       })
-      .catch(err => next(err))
-  },
-  deleteUser: (req, res, next) => {
-    return User.findByPk(req.params.id)
-      .then(user => {
-        if (!user) throw new Error("User didn't exist!")
-        return user.destroy()
-      })
-      .then(() => res.redirect('/admin/users'))
       .catch(err => next(err))
   }
 }
