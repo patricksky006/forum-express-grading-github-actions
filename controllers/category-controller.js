184a29f38351c4cf7b9c6344a39cd41b48674a9d
patricksky006 <patrick.sky@hotmail.com>
2023-09-26 19:19:48 +0800

    feat: category delete

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -44,6 +44,15 @@ const categoryController = {
         res.redirect('/admin/categories')
       })
       .catch(err => next(err))
+  },
+  deleteCategory: (req, res, next) => {
+    return Category.findByPk(req.params.id)
+      .then(Category => {
+        if (!Category) throw new Error("Category didn't exist!")
+        return Category.destroy()
+      })
+      .then(() => res.redirect('/admin/categories'))
+      .catch(err => next(err))
   }
 }
 
