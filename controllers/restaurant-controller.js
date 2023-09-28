f841b2f7f5bd53b2d31017b05a1220ed69c3a491
patricksky006 <patrick.sky@hotmail.com>
2023-09-28 11:19:32 +0800

    feat: add categories navbar on restaurants index page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -2,19 +2,30 @@ const { Restaurant, Category } = require('../models')
 
 const restaurantController = {
   getRestaurants: (req, res) => {
-    return Restaurant.findAll({
-      include: Category,
-      nest: true,
-      raw: true
-    }).then(restaurants => {
-      const data = restaurants.map(r => ({
-        ...r,
-        description: r.description.substring(0, 50) // 只取50個字，減少資源以及破版
-      }))
-      return res.render('restaurants', {
-        restaurants: data
+    const categoryId = Number(req.query.categoryId) || ''
+
+    Promise.all([
+      Restaurant.findAll({
+        include: Category,
+        where: {
+          ...categoryId ? { categoryId } : {} // 檢查 categoryId 是否為空值
+        },
+        nest: true,
+        raw: true
+      }),
+      Category.findAll({ raw: true })
+    ])
+      .then(([restaurants, categories]) => {
+        const data = restaurants.map(r => ({
+          ...r,
+          description: r.description.substring(0, 50) // 只取50個字，減少資源以及破版
+        }))
+        return res.render('restaurants', {
+          restaurants: data,
+          categoryId,
+          categories
+        })
       })
-    })
   },
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
