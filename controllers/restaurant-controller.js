8d731bf5a5f5546b0fdbbb12b151ab14bf14b2a5
patricksky006 <patrick.sky@hotmail.com>
2023-10-03 14:42:30 +0800

    Finish the R05 test

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -67,7 +67,8 @@ const restaurantController = {
     return Restaurant.findByPk(req.params.id, {
       include: [
         Category,
-        { model: Comment, include: User }
+        { model: Comment, include: User },
+        { model: User, as: 'FavoritedUsers' }
       ]
     })
       .then(restaurant => {
@@ -100,6 +101,26 @@ const restaurantController = {
           restaurants,
           comments
         })
+      })
+      .catch(err => next(err))
+  },
+  getTopRestaurants: (req, res, next) => {
+    // 撈出所有 Restaurant 與 追蹤者 資料
+    return Restaurant.findAll({
+      include: [{ model: User, as: 'FavoritedUsers' }]
+    })
+      .then(restaurants => {
+        const result = restaurants
+          .map(restaurants => ({
+            ...restaurants.toJSON(),
+            description: restaurants.description.substring(0, 50),
+            favoritedCount: restaurants.FavoritedUsers.length, // 計算追蹤者人數
+            isFavorited: req.user && req.user.FavoritedRestaurants.map(fr => fr.id).includes(restaurants.id) // 判斷目前登入使用者是否已追蹤該 restaurants 物件
+          }))
+          .sort((a, b) => b.favoritedCount - a.favoritedCount)
+          .slice(0, 10)
+
+        res.render('top-restaurants', { restaurants: result })
       })
       .catch(err => next(err))
   }
