ea44fadfe6130d9a61d79c72b7a23567cc2ec3a0
patricksky006 <patrick.sky@hotmail.com>
2023-10-07 20:43:48 +0800

    feat: add restaurant-services

--- controllers/apis/restaurant-controller.js
+++ controllers/apis/restaurant-controller.js
@@ -1,43 +1,8 @@
-const { Restaurant, Category } = require('../../models')
-const { getOffset, getPagination } = require('../../helpers/pagination-helpers')
+const restaurantServices = require('../../services/restaurant-services')
 
 const restaurantController = {
-  getRestaurants: (req, res) => {
-    const DEFAULT_LIMIT = 9
-    const categoryId = Number(req.query.categoryId) || ''
-    const page = Number(req.query.page) || 1
-    const limit = Number(req.query.limit) || DEFAULT_LIMIT
-    const offset = getOffset(limit, page)
-
-    Promise.all([
-      Restaurant.findAndCountAll({
-        include: Category,
-        where: {
-          ...categoryId ? { categoryId } : {} // 檢查 categoryId 是否為空值
-        },
-        limit,
-        offset,
-        nest: true,
-        raw: true
-      }),
-      Category.findAll({ raw: true })
-    ])
-      .then(([restaurants, categories]) => {
-        const favoritedRestaurantsId = req.user?.FavoritedRestaurants ? req.user.FavoritedRestaurants.map(fr => fr.id) : []
-        const likedRestaurantsId = req.user?.LikedRestaurants ? req.user.LikedRestaurants.map(fr => fr.id) : []
-        const data = restaurants.rows.map(r => ({
-          ...r,
-          description: r.description.substring(0, 50), // 只取50個字，減少資源以及破版
-          isFavorited: favoritedRestaurantsId.includes(r.id),
-          isLiked: likedRestaurantsId.includes(r.id)
-        }))
-        return res.json({
-          restaurants: data,
-          categoryId,
-          categories,
-          pagination: getPagination(limit, page, restaurants.count)
-        })
-      })
+  getRestaurants: (req, res, next) => {
+    restaurantServices.getRestaurants(req, (err, data) => err ? next(err) : res.json(data))
   }
 }
 
