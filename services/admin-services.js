0b88ae8195e8005a6feb605bb4fd45961bc54c93
patricksky006 <patrick.sky@hotmail.com>
2023-10-09 16:50:54 +0800

    feat: add create restaurant api

--- services/admin-services.js
+++ services/admin-services.js
@@ -1,4 +1,5 @@
 const { Restaurant, Category } = require('../models')
+const { localFileHandler } = require('../helpers/file-helpers')
 
 const adminServices = {
   getRestaurants: (req, cb) => {
@@ -17,6 +18,23 @@ const adminServices = {
         return restaurant.destroy()
       })
       .then(deletedRestaurant => cb(null, { restaurant: deletedRestaurant }))
+      .catch(err => cb(err))
+  },
+  postRestaurant: (req, cb) => {
+    const { name, tel, address, openingHours, description, categoryId } = req.body
+    if (!name) throw new Error('Restaurant name is required!')
+    const { file } = req
+    localFileHandler(file)
+      .then(filePath => Restaurant.create({
+        name,
+        tel,
+        address,
+        openingHours,
+        description,
+        image: filePath || null,
+        categoryId
+      }))
+      .then(newRestaurant => { cb(null, { restaurant: newRestaurant }) })
       .catch(err => cb(err))
   }
 }
